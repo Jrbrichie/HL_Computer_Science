e9d6ac6f0783fee80d7b63e61ab0fa297bf66367
Joshua Budiarto <jrbrichie@gmail.com>
2022-03-26 16:42:08 +0700

    26/3/22 13:35 home

--- Unit5_Arrays/Connect4/Connect4.java
+++ Unit5_Arrays/Connect4/Connect4.java
@@ -8,11 +8,14 @@ package Unit5_Arrays.Connect4;
 import java.util.Scanner;
 
 public class Connect4 {
+    //class attributes (declared private, and some final, so they
+    //are not accessed from other classes or changed
     private double money, bet;
     private String choice;
     private final int[][] board;
     private final Scanner in = new Scanner(System.in);
 
+    //constructor setting all
     public Connect4() {
         money = 0;
         bet = 0;
@@ -24,29 +27,39 @@ public class Connect4 {
         this.choice = choice;
     }
 
+    //fills board with either 1 or 2
     public void fillBoard(){
-        for(int[] row : board){
-            for(int i : row){
-                i = (int)(Math.random() * 2);
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                int num = (int)(Math.random() * 2);
+                board[i][j] = num;
             }
         }
     }
 
+    //prints all elements of the board
     public void printBoard(){
         for(int i = 0; i < 4; i++){
             for(int j = 0; j < 4; j++){
-                System.out.println(board[i][j] + " ");
+                System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
     }
+
+    /**
+     * checks if there is a vertical or horizontal match/win (more efficient than two methods)
+     * @return total amount of vertical and horizontal wins
+     */
     public int vertAndHoriWins(){
         int totalWins = 0;
         int inARowVert = 0;
         int inARowHori = 0;
+
         for(int i = 0; i < 4; i++){
             inARowVert = 0;
             inARowHori = 0;
+
             for(int j = 0; j < 4; j++){
                 if(board[j][i] == 1){
                     inARowVert++;
@@ -55,19 +68,26 @@ public class Connect4 {
                     inARowHori++;
                 }
             }
+
             if(inARowVert == 4){
                 totalWins++;
             }
             if(inARowHori == 4){
                 totalWins++;
             }
+
         }
         return totalWins;
     }
 
+    /**
+     * checks if there are diagonal wins
+     * I decided not to use a loop as there are only two possible cases (loop --> less efficient)
+     * @return total amount of diagonal wins
+     */
     public int diagonalWins(){
-        boolean cond1 = board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1 && board[3][3] == 1;
-        boolean cond2 = board[3][0] == 1 && board[2][1] == 1 && board[1][2] == 1 && board[0][3] == 1;
+        boolean cond1 = board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1 && board[3][3] == 1; //top left to bottom right
+        boolean cond2 = board[3][0] == 1 && board[2][1] == 1 && board[1][2] == 1 && board[0][3] == 1; //top right to bottom left
         int diagWins = 0;
 
         if(cond1){
@@ -79,21 +99,28 @@ public class Connect4 {
         return diagWins;
     }
 
+    /**
+     * the gameLoop() uses all relevant attributes and methods to make the test class much cleaner
+     *
+     */
     public void gameLoop() {
         System.out.print("Enter initial money at hand: ");
         money = in.nextDouble();
+
         while (money < 0) {
             System.out.print("Invalid initial money. Input another amount: ");
             money = in.nextDouble();
         }
 
         while (money > 0) {
-            System.out.print("Enter bet: ");
+            System.out.print("\nEnter bet: ");
             bet = in.nextDouble();
+
             while (bet > money || bet < 0) {
                 System.out.print("Invalid bet. Input another bet: ");
                 bet = in.nextDouble();
             }
+
             System.out.println("P - play");
             System.out.println("Q - quit");
             System.out.print("Enter your choice: ");
@@ -108,9 +135,12 @@ public class Connect4 {
                     choice = in.next();
                     invalid = !(choice.equals("P")) && !(choice.equals("Q"));
                 }
+
             } else if(choice.equals("P")){
                 fillBoard();
+                System.out.println();
                 printBoard();
+                System.out.println();
                 int connects = vertAndHoriWins() + diagonalWins();
 
                 if(connects > 0){
@@ -118,13 +148,14 @@ public class Connect4 {
                 } else {
                     money -= bet;
                 }
+
                 System.out.println("Connects: " + connects);
                 System.out.println("Current money: " + money);
             } else {
                 break;
             }
         }
-        System.out.println("You lost all of your money, or you had to go :(((");
+        System.out.println("\nYou lost all of your money, or you had to go :(((");
         System.out.println("Thanks for giving your money to us :)))");
     }
 
